4d62cc238b1ad105576ab2983d446107027e2243
aleki21 <alexkimutai021@gmail.com>
2026-06-06 15:16:42 +0300

    added js functionality to the grid

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
+const filter_btns = document.querySelectorAll(".filter-btn");
+
+filter_btns.forEach(btn => 
+    btn.addEventListener("click", () => {
+        filter_btns.forEach(button => button.classList.remove("active"));
+        btn.classList.add("active");
+
+        let filterValue = btn.dataset.filter;
+        console.log(filterValue);
+}))
+
 $('.grid').isotope({
-  // options
-  itemSelector: '.grid-item',
-  layoutMode: 'fitRows',
-  transitionDuration: "0.6s",
+    // options
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows',
+    transitionDuration: "0.6s",
 });
